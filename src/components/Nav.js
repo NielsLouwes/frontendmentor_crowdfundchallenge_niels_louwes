71c908650294782400a457b6e930d543dba1dd40
Niels Louwes <niels.louwes@and.digital>
2022-02-12 17:29:06 +0100

    added shopping cart and modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import bannerImage from '../images/image-hero-desktop.jpg'
-
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const Styles = styled.div`
     nav {
@@ -38,6 +41,17 @@ const Styles = styled.div`
         margin-left: 15px;
     }
 `
+const style = {
+  position: 'absolute',
+  top: '7%',
+  left: '70%',
+  // transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const MenuOptions = styled.div`
     display: flex;
@@ -46,20 +60,38 @@ const MenuOptions = styled.div`
     margin-right: 35px;
 `
 
-
 function Nav() {
-    return (
-        <Styles>
-            <nav style={{ backgroundImage: `url(${bannerImage})`}}>        
-                    <h3> crowdfund </h3>
-                <MenuOptions>
-                    <p className="hvr-grow">About</p>
-                    <p className="hvr-grow">Discover</p>
-                    <p className="hvr-grow">Get Started</p>
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  return (
+      <Styles>
+        <nav style={{ backgroundImage: `url(${bannerImage})`}}>        
+          <h3> crowdfund </h3>
+          <MenuOptions>
+              <p className="hvr-grow">About</p>
+              <p className="hvr-grow">Discover</p>
+              <p> <ShoppingCartOutlinedIcon onClick={handleOpen} /></p>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+           </Box>
+      </Modal>
                 </MenuOptions>
-            </nav>
+          </nav>
 
-        </Styles>
+      </Styles>
     )
 }
 
