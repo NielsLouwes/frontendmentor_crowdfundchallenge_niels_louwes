26b8a91f5bf8bea8d906a58f65a26e19ea1dca8b
Niels Louwes <niels.louwes@and.digital>
2022-03-14 13:40:23 +0100

    added test for select reward button

--- src/components/StandCard/__Test__/StandCard.test.js
+++ src/components/StandCard/__Test__/StandCard.test.js
@@ -1,48 +1,81 @@
-import { render, screen} from '@testing-library/react';
-import StandCard from '../StandCard'
+import { render, screen, fireEvent } from "@testing-library/react";
 
-describe('StandCard component', () => {
-    it('renders label of product', () => {
-        render(<StandCard label="bamboo stand" />)
-        const labelComponent = screen.getByText(/bamboo stand/i)
-        expect(labelComponent).toBeInTheDocument();
-        
-    })
+import StandCard from "../StandCard";
 
-    it('renders label of product 2', () => {
-        render(<StandCard label="black edition stand" />)
-        const labelComponent = screen.getByText(/black edition stand/i)
-        expect(labelComponent).toBeInTheDocument();
-    })
+//add dependency for userEvent
 
-     it('renders select reward button', () => {
-        render(<StandCard title="bamboo stand" />)
-        const buttonComponent = screen.getByRole('button', {  name: /select reward/i})
-        expect(buttonComponent).toBeInTheDocument();
-        // screen.debug()
-    })
+const mockIncrementButton = jest.fn();
 
-     it('renders an 64 items left', () => {
-        render(<StandCard label="bamboo stand" amountLeft="64" />)
-        const leftComponent = screen.getByText(/64/i)
-        expect(leftComponent).toBeInTheDocument();
-     })
+describe("StandCard component", () => {
+  it("renders label of product", () => {
+    render(<StandCard label="bamboo stand" />);
+    const labelComponent = screen.getByText(/bamboo stand/i);
+    expect(labelComponent).toBeInTheDocument();
+  });
 
-     it('renders left', () => {
-        render(<StandCard title="bamboo stand" />)
-        const leftComponent = screen.getByText(/left/i)
-        expect(leftComponent).toBeInTheDocument();
-    })
+  it("renders label of product 2", () => {
+    render(<StandCard label="black edition stand" />);
+    const labelComponent = screen.getByText(/black edition stand/i);
+    expect(labelComponent).toBeInTheDocument();
+  });
 
-    // it('renders -1 product left when select reward button is clicked', () => {
-    //     render(<StandCard title="bamboo stand" amountLeft= "60" />)
-    //     const leftComponent = screen.getByText(/60/i)
-    //     const buttonComponent = screen.getByRole('button', {  name: /select reward/i})
-    //     render(<StandCard title="bamboo stand" amountLeft= "61" />)
-    //     expect(leftComponent).toBeInTheDocument();
-    //     userEvent.click(buttonComponent);
-    //     expect(leftComponent.value).toBe(/59/i);
-    //     // eslint-disable-next-line testing-library/no-debugging-utils
-    //     screen.debug()
-    // })
-})
+  it("renders select reward button", () => {
+    render(<StandCard title="bamboo stand" />);
+    const buttonComponent = screen.getByRole("button", {
+      name: /select reward/i,
+    });
+
+    expect(buttonComponent).toBeInTheDocument();
+    // screen.debug()
+  });
+
+  it("renders an 64 items left", () => {
+    render(<StandCard label="bamboo stand" amountLeft="64" />);
+    const leftComponent = screen.getByText(/64/i);
+
+    expect(leftComponent).toBeInTheDocument();
+  });
+
+  it("renders left", () => {
+    render(<StandCard title="bamboo stand" />);
+    const leftComponent = screen.getByText(/left/i);
+
+    expect(leftComponent).toBeInTheDocument();
+  });
+
+  describe("Select reward button", () => {
+    it("calls increment function when clicked", () => {
+      render(
+        <StandCard
+          title="bamboo stand"
+          amountLeft={1}
+          increment={mockIncrementButton}
+        />
+      );
+
+      const buttonComponent = screen.getByRole("button", {
+        name: /select reward/i,
+      });
+
+      fireEvent.click(buttonComponent);
+      expect(mockIncrementButton).toHaveBeenCalled();
+    });
+
+    it("when amountleft is zero, button should be unclickable", () => {
+      render(
+        <StandCard
+          title="bamboo stand"
+          amountLeft={0}
+          increment={mockIncrementButton}
+        />
+      );
+
+      const buttonComponent = screen.getByRole("button", {
+        name: /select reward/i,
+      });
+      
+      fireEvent.click(buttonComponent);
+      expect(mockIncrementButton).not.toHaveBeenCalled();
+    });
+  });
+});
